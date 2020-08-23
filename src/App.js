e8485a228c2f4f8ed3977c48d8e50f0d00235ad8
dotsanjay <dotsanjay@gmail.com>
2020-08-23 12:01:27 +0800

    Created project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './homepage.component';
+import './App.css';
+import './homepage.style.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
